944bcef72cbbef36215e231726f9bcd45f9ddc12
DevooKim <khwoo8026@gmail.com>
2025-04-03 01:38:28 +0900

    feat: getSchedule 함수 추가 및 useGetSchedule 리팩토링

--- extentions/raycast-flex/src/hooks/useGetSchedule.ts
+++ extentions/raycast-flex/src/hooks/useGetSchedule.ts
@@ -53,40 +53,59 @@ const setCacheForNextMinute = (key: string, value: string) => {
   cache.set(key, value);
 };
 
+const getSchedule = async ({
+  userId,
+  cookie,
+  timestamp,
+}: {
+  userId: string;
+  cookie: string;
+  timestamp: number;
+}): Promise<ScheduleData> => {
+  const url = `https://flex.team/api/v3/time-tracking/users/${userId}/work-schedules/summary/by-working-period?timestamp=${timestamp}&timezone=Asia%2FSeoul`;
+
+  const cachedData = getCache<ScheduleData>(SCHEDULE_KEY);
+  if (cachedData) {
+    return cachedData;
+  }
+
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `AID=${cookie}`,
+    },
+  });
+
+  const fetchedData = (await response.json()) as ScheduleData;
+  const responseDate = new Date(response.headers.get("date") || "");
+
+  return {
+    ...fetchedData,
+    updatedAt: new Date(responseDate).getTime(),
+  };
+};
+
 export default function useGetSchedule() {
   const preferences = getPreferenceValues<Preferences>();
-  const cookie = preferences.cookie;
   const userId = preferences.userId;
   const timestamp = Date.now();
 
   const abortable = useRef<AbortController>(null);
 
   const result = useCachedPromise(
-    async (url: string): Promise<ScheduleData> => {
+    async (cookie): Promise<ScheduleData> => {
       const cachedData = getCache<ScheduleData>(SCHEDULE_KEY);
       if (cachedData) {
         return cachedData;
       }
 
-      const response = await fetch(url, {
-        signal: abortable.current?.signal,
-        headers: {
-          "Content-Type": "application/json",
-          Cookie: `AID=${cookie}`,
-        },
+      return await getSchedule({
+        userId,
+        cookie,
+        timestamp,
       });
-
-      const fetchedData = await response.json();
-      const responseDate = new Date(response.headers.get("date") || "");
-
-      return {
-        ...fetchedData,
-        updatedAt: new Date(responseDate).getTime(),
-      };
     },
-    [
-      `https://flex.team/api/v3/time-tracking/users/${userId}/work-schedules/summary/by-working-period?timestamp=${timestamp}&timezone=Asia%2FSeoul`, // Use userId in the URL
-    ],
+    [preferences.cookie],
     {
       initialData: "Some Text",
       abortable,
